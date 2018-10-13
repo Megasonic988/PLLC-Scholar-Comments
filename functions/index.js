eb469483a7d5ab2234c593223789af7f283c6ca8
Megasonic988 <weixiang@ualberta.ca>
2018-10-13 12:46:19 -0600

    Added lead instructors to academic emails

--- functions/index.js
+++ functions/index.js
@@ -18,10 +18,18 @@ const APP_NAME = 'TAFTA | Teaching Assistants for Teaching Assistants App - PLLC
 const wellnessDefaultEmailRecipients = ['stasia@ualberta.ca'];
 
 const academicLeadInstructors = {
-  'Foundations of Leadership (INT D 301)': [],
-  'Topics in Leadership (INT D 306)': [],
-  'Capstone in Leadership (INT D 406)': [],
-  'Workshop in Leadership (INT D 407)': []
+  'Foundations of Leadership (INT D 301)': [
+    "stasia@ualberta.ca"
+  ],
+  'Topics in Leadership (INT D 306)': [
+    "rhonda.breitkreuz@ualberta.ca"
+  ],
+  'Capstone in Leadership (INT D 406)': [
+    "ddavidso@ualberta.ca"
+  ],
+  'Workshop in Leadership (INT D 407)': [
+    "minness@ualberta.ca"
+  ]
 }
 
 exports.sendFlaggedWellnessEmail = functions.database.ref('/comments/wellness/{commentId}')
@@ -118,7 +126,7 @@ exports.sendResolvedAcademicEmail = functions.database.ref('/comments/academic/{
       return Promise.all(promises).then(snapshots => {
         const student = snapshots[0].val();
         const tf = snapshots[1].val();
-        const defaultEmailRecipients = academicLeadInstructors[comment.class] || []
+        const defaultEmailRecipients = academicLeadInstructors[afterComment.class] || []
         const emailRecipients = defaultEmailRecipients.concat(tf.email);
         const mailOptions = {
           from: `${APP_NAME} <${GMAIL_EMAIL}>`,
@@ -134,4 +142,31 @@ exports.sendResolvedAcademicEmail = functions.database.ref('/comments/academic/{
     else {
       return console.log("Academic comment not resolved. No email sent.");
     }
+  });
+
+exports.sendNoSubmissionAcademicEmail = functions.database.ref('/comments/academic/{commentId}')
+  .onCreate((snapshot) => {
+    const comment = snapshot.val();
+    if (comment.category !== 'No Submission') {
+      return console.log("Not a 'No Submission'. No email sent.");
+    }
+    const promises = [
+      admin.database().ref(`students/${comment.student}`).once('value'),
+      admin.database().ref(`users/${comment.createdBy}`).once('value')
+    ];
+    return Promise.all(promises).then(snapshots => {
+      const student = snapshots[0].val();
+      const tf = snapshots[1].val();
+      const defaultEmailRecipients = academicLeadInstructors[comment.class] || []
+      const emailRecipients = defaultEmailRecipients.concat(tf.email);
+      const mailOptions = {
+        from: `${APP_NAME} <${GMAIL_EMAIL}>`,
+        to: emailRecipients,
+        subject: `No Submission for ${student.name} in ${comment.class}`,
+        text: `A new academic comment under category ${comment.category} has been created for ${student.name}. To view this comment, please visit https://tafta-pllc.firebaseapp.com/students/${comment.student}.\n\nThanks,\nTAFTA App`
+      };
+      return mailTransport.sendMail(mailOptions).then(() => {
+        return console.log('New email sent');
+      });
+    });
   });
